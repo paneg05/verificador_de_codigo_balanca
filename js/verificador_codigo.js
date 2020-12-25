3938fde3fad7ae0486f0290739ffeda4a064e666
luan santos <aluncard6@gmail.com>
2020-12-25 11:30:29 -0300

    0.0.1

---
+++ js/verificador_codigo.js
@@ -0,0 +1,18 @@
+var tabela=[['manga',],
+            ['banana',2],
+            ['maca',3],
+            ['cenoura',4]
+            ['beteraba',5]];
+
+
+
+    var nome_produto = document.getElementById(nome_produto)
+    var tabela1=null
+
+    console.log(nome_produto)
+
+    for (i=0;i<=4;i++){
+        for (x=0;x=0;x++){
+            tabela1=tabela[i][x]
+        }
+    }
